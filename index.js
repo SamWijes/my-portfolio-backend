53e23fc4eaec83d18c3a2935c3ea09a3c44b0b3b
Samith <samithmw1000@gmail.com>
2026-04-28 17:50:10 +0530

    images added

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require('express');
 require('dotenv').config()
 const app=express();
 const PORT=3000;
+const path = require('path');
 const multer=require('multer');
+const { fileURLToPath } = require('url');
+
 
 const imageRouter = require('./routes/images');
 
+
+
 const uploads=multer({dest:'images/'});
 
-app.use('/images',imageRouter)
+// app.use('/images',imageRouter)
 
 
-
+app.use('/images',express.static(path.join(__dirname,'images')))
 
 app.listen(PORT,()=>{
     console.log("Server Listening on Port: ",PORT);
